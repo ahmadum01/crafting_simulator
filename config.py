440e4f015ba520ed5b6c0c1a7ff903d6e95a9b6f
Ahmad Umaev <ahmad.um.2001@mail.ru>
2022-08-23 23:08:32 +0300

    Refactored gonfig

--- config.py
+++ config.py
@@ -9,27 +9,11 @@ COLOR_TEXT_STATEMENT = '#242424'
 
 
 image_path = path.join('src', 'images')
-images = {
-    'a1': path.join(image_path, 'A1.png'),
-    'b1': path.join(image_path, 'B1.png'),
-    'c1': path.join(image_path, 'C1.png'),
-    'd1': path.join(image_path, 'D1.png'),
-    'e1': path.join(image_path, 'E1.png'),
-    'a2': path.join(image_path, 'A2.png'),
-    'b2': path.join(image_path, 'B2.png'),
-    'c2': path.join(image_path, 'C2.png'),
-    'd2': path.join(image_path, 'D2.png'),
-    'e2': path.join(image_path, 'E2.png'),
-    'a3': path.join(image_path, 'A3.png'),
-    'b3': path.join(image_path, 'B3.png'),
-    'c3': path.join(image_path, 'C3.png'),
-    'd3': path.join(image_path, 'D3.png'),
-    'e3': path.join(image_path, 'E3.png'),
-    'a4': path.join(image_path, 'A4.png'),
-    'b4': path.join(image_path, 'B4.png'),
-    'c4': path.join(image_path, 'C4.png'),
-    'd4': path.join(image_path, 'D4.png'),
-    'e4': path.join(image_path, 'E4.png'),
-    'serum': path.join(image_path, 'serum.png'),
-    'empty_serum': path.join(image_path, 'empty_serum.png'),
-}
+images = {}
+for letter in 'ABCDE':
+    images.update({f'{letter.lower()}{level}': path.join(image_path, f'{letter}{level}.png') for level in range(1, 5)})
+
+images['serum'] = path.join(image_path, 'serum.png')
+images['empty_serum'] = path.join(image_path, 'empty_serum.png')
+print(images)
+
